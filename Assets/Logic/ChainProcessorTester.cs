cfb047dd7a76d85787890d397137905d6c5a4dff
ukidabek <ukidabek@gmail.com>
2017-06-28 23:10:20 +0200

    - Chan Processing added.

---
+++ Assets/Logic/ChainProcessorTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BaseGameLogic.ChainProcessing;
+
+public class ChainProcessorTester : MonoBehaviour {
+
+	ChainProcessor processor = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		processor = GetComponent<ChainProcessor> ();
+
+		for (int i = 0; i < processor.Inputs.Count; i++) 
+		{
+			processor.Inputs [i].InData = new MathChainData (100);
+		}
+
+		processor.Process ();
+
+		for (int i = 0; i < processor.Outputs.Count; i++) 
+		{
+			Debug.Log ((processor.Outputs [i].OutData as MathChainData).Data.ToString ());
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
