343dff112703c032b0adfedd13bec6399648e9e5
ukidabek <ukidabek@gmail.com>
2017-06-29 21:35:39 +0200

    - Hooks added.

--- Assets/Logic/ChainProcessorTester.cs
+++ Assets/Logic/ChainProcessorTester.cs
@@ -12,23 +12,22 @@ public class ChainProcessorTester : MonoBehaviour {
 	void Start () 
 	{
 		processor = GetComponent<ChainProcessor> ();
-
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
 		for (int i = 0; i < processor.Inputs.Count; i++) 
 		{
-			processor.Inputs [i].InData = new MathChainData (100);
+			processor.Inputs [i].InData = new MathChainData (Random.Range(0f,100f));
 		}
 
 		processor.Process ();
 
 		for (int i = 0; i < processor.Outputs.Count; i++) 
 		{
-			Debug.Log ((processor.Outputs [i].OutData as MathChainData).Data.ToString ());
-		}
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+			Debug.Log ( processor.Outputs [i].IOName + ": "+
+				(processor.Outputs [i].OutData as MathChainData).Data.ToString ());
+		}	
 	}
 }
